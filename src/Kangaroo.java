1d750f964390aebb6d125296562a6142bd91a019
SureshAlien <sureshnaga96@gmail.com>
2019-05-21 17:50:18 +0530

    kangaroo tuesday

--- src/Kangaroo.java
+++ src/Kangaroo.java
@@ -3,28 +3,60 @@ public class Kangaroo {
 
 	public static void main(String[] args) 
 	{
-		int k_one_start = 0,k_two_start = 4,k_one_jump_distance = 3,k_two_jump_distance =2;
+		int k_one_start = 0,k_two_start = 3,k_one_jump_distance = 4,k_two_jump_distance =2;
 		
 		int kone_jump_count = 0;
 		int ktwo_jump_count = 0;
+		int k_one_limit,k_two_limit ;
 		long loop_count = 0 ;
 		
 		boolean check = false;
 		
-		for(long i=k_one_start, j= k_two_start ; i<= 10000 &&  j<=10000; i = i+k_one_jump_distance,j = j + k_two_jump_distance )
+		//limit calculation
+		if (k_one_start == 0 || k_one_start == 1 )
+		{
+			k_one_limit = ( k_one_start + 2 ) * (k_one_jump_distance +2);	
+		}
+		else
+		{
+			k_one_limit = k_one_start *k_one_jump_distance  ;
+			
+		}
+		if( k_two_start == 0 || k_two_start == 1   )
+		{
+			k_two_limit = ( k_two_start + 2 ) * (k_two_jump_distance +2);		
+		}
+		else
+		{
+			k_two_limit = k_two_start * k_two_jump_distance;
+			
+		}
+
+		
+		System.out.println("********************************************* limit lculation" );
+		System.out.println("k_one_limit"+k_one_limit);
+		System.out.println("k_two_limit" + k_two_limit);
+
+
+		
+		//limit calculation
+		
+		for(long i=k_one_start, j= k_two_start ; i<= k_one_limit &&  j<= k_one_limit; i = i+k_one_jump_distance,j = j + k_two_jump_distance )
 		{ 
 			kone_jump_count ++; ktwo_jump_count ++ ;
 			
 			if ( loop_count != 0)
 			{
 				if( kone_jump_count == ktwo_jump_count && i == j ) {
-					System.out.println("jumpcount"+kone_jump_count + "at location"+i+"loop_count"+loop_count+"i "+i+"j "+j);
+					System.out.println("kangaroo one  "+kone_jump_count +"\n at location: "+i);
 					check =true;	
 				}
 			}
 			loop_count ++;
 		} //end for
 		
+		System.out.println("*********************************************" );
+
 		System.out.println("loop_count"+loop_count );
 		
 		System.out.println("total jump count upto 10000 kone_jump_count"+kone_jump_count );
@@ -45,10 +77,10 @@ public class Kangaroo {
 		//debugcode
 		System.out.println("*********************************************" );
 
-		k_one_start = 0;k_two_start = 5;k_one_jump_distance = 2;k_two_jump_distance =3;
+		k_one_start = 0;k_two_start = 4;k_one_jump_distance = 4;k_two_jump_distance =2;
 		 kone_jump_count = 0;
 		 ktwo_jump_count = 0;
-		for(long i=k_one_start, j= k_two_start ; i<= 10000 &&  j<=10000; i = i+k_one_jump_distance,j = j + k_two_jump_distance )
+		for(long i=k_one_start, j= k_two_start ; i<= 1000 &&  j<=1000; i = i+k_one_jump_distance,j = j + k_two_jump_distance )
 
 		{ 
 			kone_jump_count ++; ktwo_jump_count ++ ;
@@ -77,3 +109,5 @@ public class Kangaroo {
 //0 2 5 3
 
 //0 3 4 2
+
+//4523 8092 9419 8076
